25134174fdd9bf454837978c600fc75694ad58fa
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-02-10 17:04:18 +0000

    exo2

--- exercice2.py
+++ exercice2.py
@@ -3,14 +3,17 @@ numbre = int(input("Veuillez saisir un nombre entre 10 et 20:\t "))
 nut = 10
 num = 20
 
-while numbre != 0:
-    if numbre >= num:
-        print("Plus Petit! ")
-    elif numbre <= nut:
-        print("Plus Grand! ") 
-    else:
-        print("Felicitations")
-        break
-    numbre = int(input("Veuillez saisir un nombre entre 10 et 20:\t"))
+try:
+    while numbre != 0:
+        if numbre >= num:
+            print("Plus Petit! ")
+        elif numbre <= nut:
+            print("Plus Grand! ") 
+        else:
+            print("Felicitations")
+            break
+        numbre = int(input("Veuillez saisir un nombre entre 10 et 20:\t"))
+except ValueError:
+    print("Veuillez saisir un nombre")
 
         
--- exercice3.py
+++ exercice3.py
@@ -1,62 +1,10 @@
-number1 = int(input("Veuillez saisir la premier nombre? "))
-number2 = int(input("Veuillez saisir la deuxième nombre? "))
-number3 = int(input("Veuillez saisir la troisième nombre? "))
-number4 = int(input("Veuillez saisir la Quatrième nombre? "))
-number5 = int(input("Veuillez saisir la cinquième nombre? "))
-number6 = int(input("Veuillez saisir la sixième nombre? "))
-number7 = int(input("Veuillez saisir la septième nombre? "))
-number8 = int(input("Veuillez saisir la huitième nombre? "))
-number9 = int(input("Veuillez saisir la neuvième nombre? "))
-number10 = int(input("Veuillez saisir la dixième nombre? "))
-
-dicte ={
-    1 : number1, 
-    2 : number2, 
-    3 : number3, 
-    4 : number4, 
-    5: number5, 
-    6 : number6, 
-    7 : number7, 
-    8 : number8, 
-    9: number9, 
-    10 : number10, 
-}
-
-
-
-
-
-
-
-
-# numero3 = ("3")
-
-# print(f"{numero3} : {number3}")
-
-# numero4 = ("4")
-
-# print(f"{numero4} : {number4}")
-
-# numero5 = ("5")
-
-# print(f"{numero5} : {number5}")
-
-# numero6 = ("6")
-
-# print(f"{numero6} : {number6}")
-
-# numero7 = ("7")
-
-# print(f"{numero7} : {number7}")
-
-# numero8 = ("8")
-
-# print(f"{numero8} : {number8}")
-
-# numero9 = ("9")
-
-# print(f"{numero9} : {number9}")
-
-# numero10 = ("10")
-
-# print(f"{numero10} : {number10}")
+numbre = int(input("Veuillez saisir un nombre:\t"))
+post = 1
+for position in range(2, 11):
+    numbre2 = int(input("Veuillez saisir un nombre:\t"))
+    if numbre2 > numbre :
+        numbre = numbre2
+        post = position
+print("Value", numbre)
+print('post', post )
+    
